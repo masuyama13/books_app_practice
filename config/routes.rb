69ec9ca75095de4801ffbc1c53c978d0ebffb7b5
masuyama13 <masterxwebm13@gmail.com>
2020-07-07 11:19:42 +0900

    add link to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   root "books#index"
-  get "users/:id" => "users#show"
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
