5cbb9d724eca3928fbf1167e641f1f8467a868af
masuyama13 <masterxwebm13@gmail.com>
2020-07-08 12:39:38 +0900

    add name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:zip, :address, :profile])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zip, :address, :profile])
     end
 end
