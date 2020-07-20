e25c0311954ef35dd2f7651f29cb3b6a51f8d735
masuyama13 <masterxwebm13@gmail.com>
2020-07-20 20:55:47 +0900

    add GitHub authentication by OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,25 @@
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = User.dummy_email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  def self.dummy_email
+    User.create_unique_string + "@example.com"
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
 end
