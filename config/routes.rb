ce08fb70c090f75289ac1eb8993c3dc25c4868ba
masuyama13 <masterxwebm13@gmail.com>
2020-07-28 11:38:05 +0900

    add follow/unfollow button

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   resources :books
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users, only: [:show]
   resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
