5c72de02251cb11afc5f432c68ee8ddce0d7464f
masuyama13 <masterxwebm13@gmail.com>
2020-07-30 12:29:57 +0900

    remove created_by? method

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,4 @@
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
   belongs_to :user
-
-  def created_by?(user)
-    return false if user == nil
-    user_id == user.id
-  end
 end
