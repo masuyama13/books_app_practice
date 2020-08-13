e040961926a91e00e25e3f3c83be0ea8af9e50bc
masuyama13 <masterxwebm13@gmail.com>
2020-08-13 11:35:05 +0900

    add reports system test

---
+++ test/system/reports_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class ReportsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  test "新しい日報を作成" do
+    sign_in users(:one)
+    visit new_report_path
+    within "form[name=report]" do
+      fill_in "タイトル", with: "新しい日報"
+      fill_in "本文", with: "新しい日報を書きます。"
+      click_on "登録する"
+    end
+    assert_text "日報が保存されました"
+  end
+
+  test "日報の一覧を表示" do
+    visit reports_path
+    assert_text reports(:one).title
+  end
+
+  test "日報の情報を更新" do
+    sign_in users(:one)
+    visit edit_report_path(reports(:one))
+    within "form[name=report]" do
+      fill_in "タイトル", with: "新しい日報"
+      fill_in "本文", with: "新しい日報に更新。"
+      click_on "更新する"
+    end
+    assert_text "日報が更新されました"
+  end
+
+  test "日報を削除" do
+    sign_in users(:one)
+    visit reports_path
+    accept_confirm do
+      click_on "削除"
+    end
+    assert_text "日報が削除されました"
+  end
+end
